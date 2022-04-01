2f88a48a39b678374fbb37225752c38534bc20c5
Travis Jerome Goff <99358631+VersoriumX@users.noreply.github.com>
2022-03-31 21:37:13 -0400

    adding VX genesis block

--- src/ripple/app/ledger/Ledger.cpp
+++ src/ripple/app/ledger/Ledger.cpp
@@ -181,7 +181,7 @@ Ledger::Ledger (create_genesis_t, Config const& config, Family& family)
     info_.closeTimeResolution = ledgerDefaultTimeResolution;
     static auto const id = calcAccountID(
         generateKeyPair(KeyType::secp256k1,
-            generateSeed("masterpassphrase")).first);
+            generateSeed("VersoriumX Genesis Block")).first);
     auto const sle = std::make_shared<SLE>(keylet::account(id));
     sle->setFieldU32 (sfSequence, 1);
     sle->setAccountID (sfAccount, id);
